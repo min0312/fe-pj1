0bc70793bec7a8eb11b7eb6b5d09a601d9a37764
min0312 <mkm6192@naver.com>
2023-03-24 17:20:39 +0900

    Feat: 수량 추가란 만들기

--- src/component/ShoppingListForm.tsx
+++ src/component/ShoppingListForm.tsx
@@ -1,13 +1,26 @@
+import React, { useRef } from "react";
 
-function ShoppingListForm(): JSX.Element {
+interface ShoppingListProps {
+  onAddItem: (itme: string, quantity: number) => void;
+}
+
+function ShoppingListForm({ onAddItem }: ShoppingListProps): JSX.Element {
+  const productInputRef = useRef<HTMLInputElement>(null);
+  const quantityInputRef = useRef<HTMLInputElement>(null);
+
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log("submit")
+    const newProduct = productInputRef.current!.value;
+    const quantity = parseInt(quantityInputRef.current!.value);
+    onAddItem(newProduct, quantity);
+    productInputRef.current!.value = "";
+    quantityInputRef.current!.value = "1";
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Product Name"/>
+      <input type="text" placeholder="Product Name" ref={productInputRef}/>
+      <input type="number" min={0} placeholder="Product Quantity" ref={quantityInputRef}/>
       <button type="submit">Add Item</button>
     </form>
   )
--- src/page/Page1.tsx
+++ src/page/Page1.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { v4 as getId } from "uuid";
 
 import ShoppingList from "../component/ShoppingList";
 import ShoppingListForm from "../component/ShoppingListForm";
 import Item from "../models/Item";
 
-
-
 function Page1() {
     const [items, setItems] = useState<Item[]>([]);
 
+    const addItem = (product: string, quantity: number) => {
+        console.log(product, quantity);
+        setItems([...items, { id: getId(), product, quantity }]);
+    };
     // const items = [
     //     {id: 1, product: "Lemon", quantity: 3},
     //     {id: 2, product: "Melon", quantity: 6},
@@ -17,7 +20,7 @@ function Page1() {
     return (
         <div className="Page1">
             <ShoppingList items={items}/>
-            <ShoppingListForm/>
+            <ShoppingListForm onAddItem={addItem}/>
         </div>
     )
 }
